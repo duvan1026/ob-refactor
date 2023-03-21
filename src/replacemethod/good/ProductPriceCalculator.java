3ff87813f706dd31ef7e7097992412c0b24f1481
Duvan Castro <duvancastro1026@gmail.com>
2023-03-21 04:28:51 -0500

    create class

--- src/replacemethod/good/ProductPriceCalculator.java
+++ src/replacemethod/good/ProductPriceCalculator.java
@@ -1,4 +1,22 @@
 package replacemethod.good;
 
 public class ProductPriceCalculator {
+
+    private double price;
+    private int quantity;
+
+    public ProductPriceCalculator(double price, int quantity) {
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
+    public double calculatePrice(){
+        double result = 0;
+
+        // Calculos
+
+
+        return result;
+    }
 }
