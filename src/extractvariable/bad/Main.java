c4a80a110bf6455dbfe2be4da949484311ea910b
Duvan Castro <duvancastro1026@gmail.com>
2023-03-20 21:05:46 -0500

    implement class order

--- src/extractvariable/bad/Main.java
+++ src/extractvariable/bad/Main.java
@@ -1,4 +1,14 @@
 package extractvariable.bad;
 
 public class Main {
+
+    void printProductprice(Order order){
+
+        Double totalPrioce = order.getPrice() * order.getQuantity() - order.getOfter() + order.getShipping() * 2;
+
+
+        System.out.println(totalPrioce);
+
+    }
+
 }
