22e86e16604bc47f49573ecd042cae42c3c2b346
Duvan Castro <duvancastro1026@gmail.com>
2023-03-20 20:41:50 -0500

    implement class extractclass with good practices

--- src/extractclass/good/Person.java
+++ src/extractclass/good/Person.java
@@ -6,11 +6,8 @@ public class Person {
     private String firstName;
     private String lastName;
     private Integer age;
-    //address
-    private String street;
-    private String postalCode;
-    private String city;
-    private String country;
-    private String door;
+
+    private Address address; // este atributo apunto a la clase Address
+
 
 }
