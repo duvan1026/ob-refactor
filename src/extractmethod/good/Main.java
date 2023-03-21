fd814ff78fcff0ca1437612291bd315506ad7ae1
Duvan Castro <duvancastro1026@gmail.com>
2023-03-20 20:52:38 -0500

    file create

---
+++ src/extractmethod/good/Main.java
@@ -0,0 +1,24 @@
+package extractmethod.good;
+
+public class Main {
+
+    void printHTML(){
+
+        printHead();
+        printBody();
+        System.out.println("This is the footer");
+        System.out.println("script");
+        System.out.println("copyright");
+        System.out.println("");
+
+    }
+
+    private void printBody(){
+
+    }
+
+    private void printHead(){
+
+    }
+
+}
