7d694f0ad8f90dfa644c1efc247bf7d20ed13279
Duvan Castro <duvancastro1026@gmail.com>
2023-03-20 20:54:14 -0500

    implement class extractmethod with good practices

--- src/extractmethod/good/Main.java
+++ src/extractmethod/good/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
         printHead();
         printBody();
+        printFooter();
+
+    }
+
+    private void printFooter() {
         System.out.println("This is the footer");
         System.out.println("script");
         System.out.println("copyright");
         System.out.println("");
-
     }
 
     private void printBody(){
