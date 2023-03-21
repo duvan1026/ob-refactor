69ebe2ffe006d7523656ad75fedb043f9b3daa3a
Duvan Castro <duvancastro1026@gmail.com>
2023-03-21 04:08:50 -0500

    implement class Oder

--- src/removeassign/bad/Main.java
+++ src/removeassign/bad/Main.java
@@ -1,14 +1,16 @@
 package removeassign.bad;
 
+import extractvariable.bad.Order;
+
 public class Main {
 
-    double calculateDiscount( Order order, double totalPrice ){
+    double calculateDiscount(Order order, double totalPrice ){
 
         if(order == null || order.getPrice()  == null) // programación defensiva
             return totalPrice;
 
-        if( order.getPrice > 100 )
-            totalPrice += order.getPrice() * order.getOffer(); // sobreescribe totalPrice
+        if( order.getPrice() > 100 )
+            totalPrice += order.getPrice() * order.getOfter(); // sobreescribe totalPrice
 
         return totalPrice;
     }
